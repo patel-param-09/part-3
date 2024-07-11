24a8e725004212dd3cc14904d6c32ecfa4c75ba2
patel-param-09 <patelparam9304@gmail.com>
2024-07-11 16:24:44 +0530

    3.3: Phonebook backend step 3

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 const express = require('express')
 const app = express()
 
-const contact = [
+let persons = [
     { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
-app.get('/',(req, res)=>{
-    res.send(`<p>The PhoneBook have info for 4 people </br> </br> ${Date()} </p>`)
+
+app.get('/api/persons/:id',(req, res)=>{
+    const id = req.params.id
+    const person = persons.find(person => person.id === id)
+    res.send(person)
 })
 
-app.listen(3000)
+app.listen(3001)
