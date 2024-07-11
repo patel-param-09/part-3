7d89d18b10ef5c33d0caafec8826cbe71b800316
patel-param-09 <patelparam9304@gmail.com>
2024-07-11 17:47:06 +0530

    3.7: Phonebook backend step 7 (Just install morgan package) 3.8*: Phonebook backend step 8

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
 
 let persons = [
     { 
