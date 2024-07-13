622ec41a4ca4e689fe7a3d6b367dd070f4d9b80d
patel-param-09 <patelparam9304@gmail.com>
2024-07-13 17:24:58 +0530

    3.14: Phonebook database, step 2

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ app.get('/', (req,res)=>{
   res.send("hello")
 })
 
+app.get('/create', async(req, res)=>{
+  let phonebookdata = await phonebookModel.create({
+   id:3,
+   name:"Arto Vihavainen",
+   number: 45-1232456
+  })
+  res.send(phonebookdata)
+ })
+ 
 app.listen(3000)
